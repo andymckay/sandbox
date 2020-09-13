9c8a91760d63933e2038dfe232e994f8f8a2f08e
Andy McKay <andymckay@github.com>
2020-09-13 09:37:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
 
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { correctRecipients, correctMessage } = require('./utils');
+const labelsToAdd = core.getInput("label");
 
-async function run() {
-  try {
-    const number = github.context.payload.issue.number || github.context.payload.pull_request.number;
-    const owner = github.context.repo.owner;
-    const repo = github.context.repo.repo;
-    const label = github.context.payload.label.name;
+async function label() {
+  const myToken = core.getInput("repo-token");
+  const octokit = new github.GitHub(myToken);
+  const context = github.context;
 
-    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const token = core.getInput('token');
-    const octokit = new github.GitHub(token);
 
-    const labelRecipients = core.getInput('recipients').split("\n");
-    const match = labelRecipients.find((labelRecipient) => {
-      return labelRecipient.split("=")[0] === label;
-    });
-
-    const message = core.getInput('message');
-
-    if (match) {
-      const recipients = correctRecipients(match.split("=")[1]);
-      const comment = correctMessage(message, recipients, label);
-      const createCommentResponse = await octokit.issues.createComment({
-        owner,
-        repo,
-        issue_number: number,
-        body: comment
-      });
-    } else {
-      console.log("No matching recipients found for label ${label}.");
-    }
-  } catch (error) {
-    console.error(error);
-    core.setFailed(`The issue-label-notification-action action failed with ${error}`);
-  }
+  let timeline = await octokit.issues.listEventsForTimeline({
+    owner: context.payload.repository.owner.login,
+    repo: context.payload.repository.name,
+    issue_number: context.payload.issue.number,
+  });
+  context.log(timeline);
 }
 
-run();
+comment()
+  .then(
+    result => {
+      // eslint-disable-next-line no-console
+      console.log(`Complete.`);
+    },
+    err => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+    }
+  )
+  .then(() => {
+    process.exit();
+  });
